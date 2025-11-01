aa11a81939c83d862ec1b04847183c3df6d289c6
gokul <gokul6670@gmail.com>
2025-11-01 17:55:43 +0530

    Initial Commit

---
+++ screengate/screengateApp.swift
@@ -0,0 +1,17 @@
+//
+//  screengateApp.swift
+//  screengate
+//
+//  Created by gokul on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct screengateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
